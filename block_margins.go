a1c8b15c514423b1cebebcd6bbb4ea4e9c4029dc
Carlos Forero <ecarlosforero@gmail.com>
2023-07-27 19:14:32 +0200

    Remove Width prefix from Margin properties

--- block_margins.go
+++ block_margins.go
@@ -4,39 +4,39 @@ import (
 	"github.com/kjk/flex"
 )
 
-func (block *Block) MarginTopWidth(width float64) *Block {
+func (block *Block) MarginTop(width float64) *Block {
 
 	block.getFlexNode().StyleSetMargin(flex.EdgeTop, float32(width))
 
 	return block
 }
 
-func (block *Block) MarginBottomWidth(width float64) *Block {
+func (block *Block) MarginBottom(width float64) *Block {
 
 	block.getFlexNode().StyleSetMargin(flex.EdgeBottom, float32(width))
 
 	return block
 }
 
-func (block *Block) MarginLeftWidth(width float64) *Block {
+func (block *Block) MarginLeft(width float64) *Block {
 
 	block.getFlexNode().StyleSetMargin(flex.EdgeLeft, float32(width))
 
 	return block
 }
 
-func (block *Block) MarginRightWidth(width float64) *Block {
+func (block *Block) MarginRight(width float64) *Block {
 
 	block.getFlexNode().StyleSetMargin(flex.EdgeRight, float32(width))
 
 	return block
 }
 
-func (block *Block) MarginWidth(width float64) *Block {
-	block.MarginTopWidth(width)
-	block.MarginRightWidth(width)
-	block.MarginBottomWidth(width)
-	block.MarginLeftWidth(width)
+func (block *Block) Margin(width float64) *Block {
+	block.MarginTop(width)
+	block.MarginRight(width)
+	block.MarginBottom(width)
+	block.MarginLeft(width)
 
 	return block
 }
