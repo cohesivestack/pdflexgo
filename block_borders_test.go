d41194e5850975514a866067b4bfd0b016edc559
Miguel Forero Vasquez <miguel@pop-os.localdomain>
2023-07-28 10:39:17 -0500

    Add Tests of Width to Margin and Border

--- block_borders_test.go
+++ block_borders_test.go
@@ -7,14 +7,54 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPageBorderWidth(t *testing.T) {
+func TestPageMarginWidth(t *testing.T) {
 
 	page := NewPage().
-		BorderWidth(5)
+		Border(5, 5, 5, 5)
 
 	assert.Equal(t, float32(5), page.root.getFlexNode().StyleGetBorder(flex.EdgeTop))
 	assert.Equal(t, float32(5), page.root.getFlexNode().StyleGetBorder(flex.EdgeRight))
 	assert.Equal(t, float32(5), page.root.getFlexNode().StyleGetBorder(flex.EdgeBottom))
 	assert.Equal(t, float32(5), page.root.getFlexNode().StyleGetBorder(flex.EdgeLeft))
+
+	page = NewPage().
+		BorderTop(6)
+
+	assert.Equal(t, float32(6), page.root.getFlexNode().StyleGetBorder(flex.EdgeTop))
+
+	page = NewPage().
+		BorderRight(6)
+
+	assert.Equal(t, float32(6), page.root.getFlexNode().StyleGetBorder(flex.EdgeRight))
+
+	page = NewPage().
+		BorderBottom(6)
+
+	assert.Equal(t, float32(6), page.root.getFlexNode().StyleGetBorder(flex.EdgeBottom))
+
+	page = NewPage().
+		BorderLeft(6)
+
+	assert.Equal(t, float32(6), page.root.getFlexNode().StyleGetBorder(flex.EdgeLeft))
+
+	page = NewPage().
+		BorderVertical(7)
+
+	assert.Equal(t, float32(7), page.root.getFlexNode().StyleGetBorder(flex.EdgeTop))
+	assert.Equal(t, float32(7), page.root.getFlexNode().StyleGetBorder(flex.EdgeBottom))
+
+	page = NewPage().
+		BorderHorizontal(7)
+
+	assert.Equal(t, float32(7), page.root.getFlexNode().StyleGetBorder(flex.EdgeRight))
+	assert.Equal(t, float32(7), page.root.getFlexNode().StyleGetBorder(flex.EdgeLeft))
+
+	page = NewPage().
+		BorderAll(6)
+
+	assert.Equal(t, float32(6), page.root.getFlexNode().StyleGetBorder(flex.EdgeTop))
+	assert.Equal(t, float32(6), page.root.getFlexNode().StyleGetBorder(flex.EdgeRight))
+	assert.Equal(t, float32(6), page.root.getFlexNode().StyleGetBorder(flex.EdgeBottom))
+	assert.Equal(t, float32(6), page.root.getFlexNode().StyleGetBorder(flex.EdgeLeft))
 
 }
